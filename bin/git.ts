f50bbab124cb77a7af5f3bfa4fc40884b550e256
Lukáš Průša <lukyn.patai5@gmail.com>
2026-01-28 18:24:02 +0100

    fix: file diffs between commits

--- bin/git.ts
+++ bin/git.ts
@@ -4,49 +4,34 @@ import { spawnCommandInGhWorkspace } from './utils.js';
 export const GIT_FORMAT_SEPARATOR = '»¦«';
 const GIT_LOG_FORMAT = ['%H', '%aN<%aE>', '%aD', '%s'].join(GIT_FORMAT_SEPARATOR);
 
-export const getBranchLastCommit = (branch: string): Commit => {
-    const commitString = spawnCommandInGhWorkspace(`git log -1 --pretty=format:'${GIT_LOG_FORMAT}' ${branch}`);
-    const commit = parseCommit(commitString);
-    return commit;
-};
-
+/**
+ * Gets the list of changed files between the given commits (inclusive).
+ */
 export const getChangedFiles = (commits: Commit[]) => {
-    const changedFiles =
-        commits.length === 1
-            ? spawnCommandInGhWorkspace(`git show --pretty='format:' --name-only ${commits[0].sha}`)
-            : spawnCommandInGhWorkspace(`git diff --name-only ${commits[0].sha}..${commits[commits.length - 1].sha}`);
+    const changedFiles = spawnCommandInGhWorkspace(
+        `git diff --name-only ${commits[0].sha}~..${commits[commits.length - 1].sha}`,
+    );
+
     return changedFiles.split('\n');
 };
 
+/**
+ * Gets the commits between sourceBranch and targetBranch (exclusive).
+ * - If baseCommit is provided, only returns commits after the baseCommit.
+ */
 export const getCommits = ({ sourceBranch, targetBranch, baseCommit: baseCommitSha }: Config): Commit[] => {
-    const targetBranchCommit = getBranchLastCommit(targetBranch);
-    const sourceBranchCommit = getBranchLastCommit(sourceBranch);
-    let baseCommit: Commit | undefined;
-    if (baseCommitSha) {
-        try {
-            baseCommit = getCommitInfo(baseCommitSha);
-        } catch (err) {
-            console.warn(`Failed to get base commit "${baseCommitSha}", it will not be used: ${err}`);
-        }
-    }
-    // const baseCommit = baseCommitSha !== undefined ?  : undefined;
-    // const gitOutputFormat = '{"sha":"%H","author":"%aN<%aE>","date":"%aD","message":"%f"}';
-    // console.log(`git log --pretty=format:"${gitOutputFormat}" ${targetBranch}..${sourceBranch}`);
-    // return [];
-    const base = getBaseCommit(sourceBranchCommit, targetBranchCommit);
     const commitsStrings = spawnCommandInGhWorkspace(
-        `git log --pretty=format:'${GIT_LOG_FORMAT}' ${base.sha}..${sourceBranchCommit.sha}`
+        `git log --pretty=format:'${GIT_LOG_FORMAT}' ${targetBranch}..${sourceBranch}`,
     ).split('\n');
-    const commits = commitsStrings.map((commitString) => parseCommit(commitString)) as Commit[];
+    const commits = commitsStrings.map((commitString) => parseCommit(commitString));
     commits.reverse();
-    const baseCommitIndex = baseCommit ? commits.findIndex(({ sha }) => sha === baseCommit.sha) : -1;
-    return commits.slice(baseCommitIndex + 1);
-};
 
-export const getBaseCommit = (target: Commit, source: Commit): Commit => {
-    const baseCommitSha = spawnCommandInGhWorkspace(`git merge-base ${target.sha} ${source.sha}`);
-    const baseCommit = getCommitInfo(baseCommitSha);
-    return baseCommit;
+    const baseCommitIndex = commits.findIndex((commit) => commit.sha === baseCommitSha);
+
+    const hasBaseCommit = baseCommitIndex !== -1;
+    if (hasBaseCommit) return commits.slice(baseCommitIndex + 1);
+
+    return commits;
 };
 
 export const getCommitInfo = (commitSha: string): Commit => {
